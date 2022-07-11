acbe3ee4e76d4b05e053c99f30ea5a21830dea7f
Rishikesh Thote <r.thote@zensar.com>
2022-07-11 11:23:26 +0530

    FinalCodbase_Evaluation

---
+++ FinalCodbase_Evaluation/CodbaseEvaluation/CodbaseEvaluation/Data/CodbaseEvaluationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodbaseEvaluation.Model;
+
+namespace CodbaseEvaluation.Data
+{
+    public class CodbaseEvaluationContext : DbContext
+    {
+        public CodbaseEvaluationContext (DbContextOptions<CodbaseEvaluationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodbaseEvaluation.Model.Employee> Employee { get; set; }
+    }
+}
---
+++ FinalCodbase_Evaluation/CodbaseEvaluation/CodbaseEvaluation/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodbaseEvaluation.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string EmpDept { get; set; }
+        [Required]
+        public string EmpName { get; set; }
+        public string Gender { get; set; }
+ 
+        public string Location { get; set; }
+    }
+}
